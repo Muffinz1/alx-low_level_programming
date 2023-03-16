09123a24d7a87969d62a5d078d7df9f532ed85e7
Amir Youssef <amiryou120@gmail.com>
2023-03-16 15:29:44 -0700

    size of multi variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Size of function usage
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	printf("Size of a char:%ubyte(s)\n"), sizeof(char);
+	printf("Size of an int:%ubyte(s)\n"), sizeof(int);
+	printf("Size of a long int:%ubyte(s)\n"), sizeof(long int);
+	printf("Size of a long long int:%ubyte(s)\n"), sizeof(long long int);
+	printf("Size of a float:%ubyte(s)\n"), sizeof(float);
+	return (0);
+}
