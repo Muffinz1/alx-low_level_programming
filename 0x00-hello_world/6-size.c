fbde9d78af46b70a5ea43d042220b93fa9e82b98
Amir Youssef <amiryou120@gmail.com>
2023-03-16 15:34:37 -0700

    size of multi variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-	printf("Size of a char:%ubyte(s)\n"), sizeof(char);
-	printf("Size of an int:%ubyte(s)\n"), sizeof(int);
-	printf("Size of a long int:%ubyte(s)\n"), sizeof(long int);
-	printf("Size of a long long int:%ubyte(s)\n"), sizeof(long long int);
-	printf("Size of a float:%ubyte(s)\n"), sizeof(float);
+	printf("Size of a char:%ubyte(s)\n", sizeof(char));
+	printf("Size of an int:%ubyte(s)\n", sizeof(int));
+	printf("Size of a long int:%ubyte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%ubyte(s)\n", sizeof(long long int));
+	printf("Size of a float:%ubyte(s)\n", sizeof(float));
 	return (0);
 }
