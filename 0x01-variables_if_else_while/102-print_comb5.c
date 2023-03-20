9d6652ebabeacb28925959409b04e7d4ef191f40
Amir Youssef <amiryou120@gmail.com>
2023-03-20 16:09:11 -0700

    name is four

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Description: printing 4 combinations
+ *
+ * Return: 0 (always success)
+ *
+*/
+int main(void)
+{
+	int Num1 = 0;
+	int Num2;
+
+	while (Num1 <= 99)
+	{
+		Num1 = Num2;
+
+			while (Num2 <= 99)
+			{
+				if (Num2 != Num1)
+				{
+				putchar((Num1 / 10) + 48);
+				putchar((Num1 % 10) + 48);
+				putchar(' ');
+				putchar((Num2 / 10) + 48);
+				putchar((Num2 % 10) + 48);
+					if (Num1 != 98 || Num2 != 99)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				Num2++;
+			}
+			Num1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
