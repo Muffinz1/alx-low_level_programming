bad81e2b8901ad43199ed61d485c88bb55b1daa4
Amir Youssef <amiryou120@gmail.com>
2023-03-20 14:04:33 -0700

    check and print last char

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,6 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	number = n % 10;
 	if (number > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, number);
 	else if	(number == 0)
