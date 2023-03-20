08ae1d59ca45fcb48cf96ea43ec6d3d930da141b
Amir Youssef <amiryou120@gmail.com>
2023-03-20 14:37:07 -0700

    numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: for char put
+ *
+ * Return: 0 (always success)
+*/
+
+int main(void)
+{	int X = '0';
+	while (X <= '0')
+	{
+		putchar (X);
+		X++;
+	}
+	putchar ('\n');
+	return (0);
+}
