239fdb3e6a862075fc5899a7d8445cc4c6a72ff1
Amir Youssef <amiryou120@gmail.com>
2023-03-20 14:59:11 -0700

    hexadecimals are here

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: hexadecimal
+ *
+ * Return: 0 (always success)
+*/
+
+int main(void)
+{	int number = 48;
+	while (number <= 102)
+	{
+		putchar(number);
+		if(number == 57)
+			number += 39;
+		number++
+	}
+	putchar ('\n');
+	return (0);
+}
