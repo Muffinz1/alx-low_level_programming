bfa19634a73b1730d6a4a79094a1d1289c84fb2b
Amir Youssef <amiryou120@gmail.com>
2023-03-22 15:13:16 -0700

    absolutely

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ *
+ * @n: the int to check
+ *
+ * Return: 0 (always success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	n = (-1) * n;
+	return (0);
+}
