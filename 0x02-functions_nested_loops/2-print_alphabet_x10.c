79a0957fe009ce488a9ab1a446fb4f75f9a52952
Amir Youssef <amiryou120@gmail.com>
2023-03-22 14:29:55 -0700

    10 times more

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10-uses _putchar
+ *
+ * Description: prints alphabet
+ *
+ * Return: 0 (always success)
+*/
+void print_alphabet_x10(void)
+{
+	int x, line;
+		for (line = 0; line <= 9 ; line++)
+		{
+			for (x = 'a'; x <= 'z'; x++)
+				_putchar(x);
+				_putchar('\n');
+		}
+
+}
