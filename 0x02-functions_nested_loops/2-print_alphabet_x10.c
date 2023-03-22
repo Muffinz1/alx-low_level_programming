72e584777003e0221fac9bfdc69783da9c103739
Amir Youssef <amiryou120@gmail.com>
2023-03-22 14:33:49 -0700

    10 times more

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,8 @@
 */
 void print_alphabet_x10(void)
 {
-	int x, line;
-		for (line == 0; line <= 9 ; line++)
+	int line, x;
+		for (line = 0; line <= 9 ; line++)
 		{
 			for (x = 'a'; x <= 'z'; x++)
 				_putchar(x);
