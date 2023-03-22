1a59a453c194e66407ad781efad517ecbae99dfb
Amir Youssef <amiryou120@gmail.com>
2023-03-22 12:51:53 -0700

    char functionz

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main-entry point
+ *
+ * Description: prints function in main
+ *
+ * return: 0 (always success)
+*/
+int main(void)
+{	char str[] = "_putchar";
+	int x;
+	for (x = 0; x < 8;x++ )
+		_putchar(str[x]);
+	_putchar('\n')
+		return(0);
+}
