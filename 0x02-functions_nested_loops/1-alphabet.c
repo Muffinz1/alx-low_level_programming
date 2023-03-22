e537ad4a1131e5103fcbaf0b21e9bef9de2c3f55
Amir Youssef <amiryou120@gmail.com>
2023-03-22 14:19:45 -0700

    my letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet-uses _putchar
+ *
+ * Description: prints alphabet
+ *
+ * Return: 0 (always success)
+*/
+int print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+			_putchar('\n');
+
+}
