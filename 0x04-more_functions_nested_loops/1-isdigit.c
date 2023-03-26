60e562fd22665e3ecd12de19d7d6658b938e4ebd
Amir Youssef <amiryou120@gmail.com>
2023-03-26 03:20:37 -0700

    prints digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - function for checking char
+ *
+ * @c:the int c
+ *
+ * Return: 1(always upper) or 0 (always lower)
+*/
+int _isdigit(int c)
+{
+        if (c >= 48 && c <= 57)
+                {
+                return (1);
+                }
+        else
+        return (0);
+}
