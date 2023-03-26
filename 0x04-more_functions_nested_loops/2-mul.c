6b97b3b4d1c509079ca6fcab99c7911b975eb2aa
Amir Youssef <amiryou120@gmail.com>
2023-03-26 05:48:59 -0700

    my multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * mul(int a, int b)- multiplication of two numbers
+ *
+ * @a: first number to be multiplied
+ *
+ * @b: second number to be multiplied
+ *
+ * Return: 0 (always success)
+*/
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	printf("%i", multi);
+	return (0);
+}
