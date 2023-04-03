0ed4397e08e9207147bc69993a18d8a40d8ceaef
Amir Youssef <amiryou120@gmail.com>
2023-04-03 13:08:15 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - draw lines
+ *
+ * @n: number of lines
+ *
+ * Return: 0 (always success)
+ *
+*/
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	while (n > 0)
+	{
+	_putchar(95);
+	n = n - 1;
+	}
+	_putchar('\n');
+}
