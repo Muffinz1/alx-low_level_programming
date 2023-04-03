8cd4112aadb8a66c2490ef53ae7017080a22f8be
Amir Youssef <amiryou120@gmail.com>
2023-04-03 10:58:59 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers
+ *
+ * Description: prints numbers 14 times
+*/
+
+void more_numbers(void)
+{
+	for (i = 0; i < 14; i++)
+	{
+		int Num = 0;
+
+		while (Num < 15)
+		{
+			if (Num / 10 != 0)
+			{
+			_putchar(Num % 10 + 48);
+			}
+			_putchar(Num % 10 + 48);
+			Num++;
+		}
+		Num = 0;
+		_putchar('\n');
+	}
+}
