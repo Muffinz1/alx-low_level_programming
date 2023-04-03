4875c82c3b9ddbf2ab92e242d5e638f2e1f07c33
Amir Youssef <amiryou120@gmail.com>
2023-04-03 02:39:29 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * print_most_numbers - prints numbers
+ *
+ * Description: prints numbers except 0,4
+*/
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		if (num != 2 || num != 4)
+		{
+			_putchar(num + 45);
+			num++
+		}
+		else
+		{
+			num++;
+		}
+		_putchar('\n');
+	}
+}
