f4d34f2ec02e3904408f4204d01dc08078b1aafd
Amir Youssef <amiryou120@gmail.com>
2023-04-04 11:53:50 -0700

    print diagnonalz

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - function
+ *
+ * @n : number of dashes to be printed
+ *
+ * Description : prints diagonal line
+ *
+*/
+
+void print_diagonal(int n)
+{
+	int space = 0;
+	int dash = 0;
+
+	while (dash < n)
+	{
+		while (space < dash)
+		{
+		_putchar(32);
+		space++;
+		}
+	_putchar(92);
+	_putchar('\n');
+	dash = dash + 1;
+	space = 0;
+	}
+}
