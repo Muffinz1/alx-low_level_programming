52ca7cd2b677a1b4d46c65755a4de407842a5348
Amir Youssef <amiryou120@gmail.com>
2023-04-04 14:03:54 -0700

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - draws a square
+ *
+ * @size: takes size as input
+ *
+ * Return: 0 (always success)
+*/
+void print_square(int size)
+{
+	int unit = 0;
+	int square = 0;
+
+	while (square < size)
+	{
+	while (unit < size)
+	{
+	_putchar('#');
+	unit++;
+	}
+	if (unit == size)
+	{
+	_putchar('\n');
+	}
+	unit = 0;
+	square++;
+	}
+}
