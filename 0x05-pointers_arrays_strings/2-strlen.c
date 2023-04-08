dc04765b18ca9e23e5abe95ff6c04930faccbcd9
Amir Youssef <amiryou120@gmail.com>
2023-04-08 04:09:19 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - function for length
+ *
+ * @s: input array
+ *
+ * Return: length (always success)
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	while (*s != '\0')
+	{
+		*s++;
+		length++;
+	}
+	return (length);
+}
