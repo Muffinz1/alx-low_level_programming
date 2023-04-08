e88bc2429c3f62450d2c0d4f2fee3939ed760ac2
Amir Youssef <amiryou120@gmail.com>
2023-04-08 06:10:29 -0700

    reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - rev the array
+ *
+ * @s: input array
+*/
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	while (length >= 0)
+	{
+	_putchar(*s--);
+	length--;
+	}
+}
