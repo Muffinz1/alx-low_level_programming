7c599cb4d11021d2abe3c2927fa139e57bae34df
Amir Youssef <amiryou120@gmail.com>
2023-04-10 15:59:29 -0700

    arguement counts

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point for agrc values
+ *@argc: arguement count
+ *@argv: arguement array values
+ * Return: 0 (always success)
+*/
+
+int main(int argc, char **argv)
+{
+	void(**argv);
+
+	printf("%s\n", argc - 1);
+	return (0);
+}
