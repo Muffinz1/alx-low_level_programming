75dee7d1ccf4d6a55a087e94a501c7811a67d81c
Amir Youssef <amiryou120@gmail.com>
2023-04-10 15:35:11 -0700

    my name betty fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,12 @@
  * main - entry point for argc and argv
  *@argc: arguement counts
  *@argv: arguement value array
+ *Return:0 (always success)
 */
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
-	return(0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
