7b5f8fd4189490249efd3a0ff1237db74af1090e
Amir Youssef <amiryou120@gmail.com>
2023-04-11 16:58:17 -0700

    array making

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,9 +10,9 @@
 */
 char *create_array(unsigned int size, char c)
 {
-	char *s = (char*) malloc(size);
+	char *s = (char *) malloc(size);
 
-	if (size == 0)
+	if (size == 0 || s == 0)
 	{
 		return (0);
 	}
