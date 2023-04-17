fea1499eeb24008580e707a0078a0c7c0b6777cc
Amir Youssef <amiryou120@gmail.com>
2023-04-17 07:46:13 -0700

    my file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point funtion
+ *
+ * Description: prints the name of the file
+ *
+ * Return: 0 (always success)
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
