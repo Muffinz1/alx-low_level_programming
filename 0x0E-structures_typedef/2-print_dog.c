076ceb55f3964a65e256d9e2ae288ccd4c4ce4ab
Amir Youssef <amiryou120@gmail.com>
2023-04-17 18:23:58 -0700

    dog or nil

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - function prints dogs data
+ *@d: dog data
+ *
+ *Description: prints dog data with pointer address
+*/
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age:%i\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+
+	}
+
+}
