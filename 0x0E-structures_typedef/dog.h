59fe95ddcd811234d5a90b49ef55221f944ab100
Amir Youssef <amiryou120@gmail.com>
2023-04-17 18:03:54 -0700

    dog id card

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure for dog data
+ *
+ * @name: dog name entry
+ * @age: float for age of the dog
+ * @owner: owner name for information
+ *
+ * Description: ID for dogs in the dog land
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
