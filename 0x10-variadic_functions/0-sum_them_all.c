3a870778d3b518b824abbbf9e68b52a3ab5f57c2
Amir Youssef <amiryou120@gmail.com>
2023-04-20 11:13:30 -0700

    sum of all o.o !

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - a function to sum every input
+ *
+ * @n:arguments input count
+ * @...:rest of args values
+ *
+ * Return: 0 if n == 0, else integer sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i = n;
+	va_list list;
+
+	if (n == 0)
+		return (0)
+
+	va_start(list, n);
+
+	while (i--)
+	{
+	sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+This is a read me file that has the scripts descriptions
+1- first file prints the sum of every thing entered to it 
