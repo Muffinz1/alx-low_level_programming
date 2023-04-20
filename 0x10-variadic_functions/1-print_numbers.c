b3926f953f2685e944c37e8fdc8b64cc9b6e730a
Amir Youssef <amiryou120@gmail.com>
2023-04-20 14:21:30 -0700

    numbers with separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function that prints numbers and follows by a new line
+ *
+ * @separator: the string to be printed between numbers
+ * @n: arguement counts for var
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int count = n;
+	va_list list;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(list, n);
+	while (count--)
+	{
+	printf("%i%s", va_arg(list, int),
+	count ? (separator ? separator : "") : "\n");
+	}
+	va_end(list);
+}
