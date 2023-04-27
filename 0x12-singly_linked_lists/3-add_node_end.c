22bd65eb696ffc5cc2e6d052fbd9022e73bc53bf
Muffinz1 <amiryou120@gmail.com>
2023-04-27 16:06:36 -0700

    adding a tail

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- *add_node - adds a node to this linked list
+ *add_node_end - adds a node in the last "tail" to this linked list
  *
  *@head: it includes the pointer address
  *@str: strings
@@ -12,19 +12,22 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_tail = malloc(sizeof(list_t));
 	list_t *tail = *head;
+
 	if (!head || !new_tail)
 		return (0);
 	if (str)
 	{
 		new_tail->str = strdup(str);
+
 		if (!new_tail->str)
 		{
 		free(new_tail);
 		return (0);
 		}
+
 		new_tail->len = stlen(new_tail->str);
 	}
-	if(tail)
+	if (tail)
 	{
 		while (tail->next)
 			tail = tail->next;
