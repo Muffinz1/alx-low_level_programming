60ada72f477aa7bb11e35442976cd4141235cc28
Muffinz1 <amiryou120@gmail.com>
2023-05-04 14:43:57 -0700

    index located !

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - Function that returns the value of a bit
+ * at a given index
+ *
+ * @n: input integer
+ * @index: index number
+ * Return: -1 (Fail), value of bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+	{
+		return (-1);
+	}
+	return (n >> index & 1);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -4,3 +4,5 @@ this read me file is used to describe the task script
 0- script is used to convert a binary number to an unsigned int.
 
 1- script prints the binary representation of a number.
+
+2- script that prints the value of a bit at a given index.
