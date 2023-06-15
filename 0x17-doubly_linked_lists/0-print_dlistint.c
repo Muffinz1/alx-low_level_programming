2226476cd3521f1194b910bd8dd68f456f97e419
Muffinz1 <amiryou120@gmail.com>
2023-06-15 11:45:33 -0700

    my first double links printing

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - a function that prints the double list
+ *
+ * @h: input pointer
+ *
+ * Return: the number
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	int num = 0;
+
+	if (h == 0)
+	return (num);
+	while (h->prev != 0)
+	{
+	h = h->prev
+	}
+	while (h != 0)
+	{
+	printf("%d\n", h->n)
+	num++;
+	h = h->next;
+	}
+	return (num);
+}
