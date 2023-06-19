ccbeddd794e5462876c64c8beb4cc233b45f2c69
Muffinz1 <amiryou120@gmail.com>
2023-06-19 13:24:01 -0700

    operations heho!

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * add - a functin that adds
+ * @a: first number
+ * @b: second number
+ * Return: the sum as result
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - a functin that sub
+ * @a: first number
+ * @b: second number
+ * Return: the diff as result
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - a functin that multi
+ * @a: first number
+ * @b: second number
+ * Return: the result
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - a functin that divides
+ * @a: first number
+ * @b: second number
+ * Return: the result
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - a functin that gets the mod
+ * @a: first number
+ * @b: second number
+ * Return: the result
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
